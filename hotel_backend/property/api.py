86de005e424be1d9d2199c5895a4feb2371f96c0
Harshvardhan Vatsa <harshvardhanvatsa@gmail.com>
2024-12-21 01:41:32 +0530

    Created API endpoints and fixed auth issues regarding the post meth of properties

--- hotel_backend/property/api.py
+++ hotel_backend/property/api.py
@@ -1,24 +1,36 @@
-from django.http  import JsonResponse
+from django.http import JsonResponse
 
-from rest_framework.decorators import api_view,authentication_classes,permission_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
 from .forms import PropertyForm
 from .models import Property
-from .serializers import PropertiesListSerializer
-from useraccount.models import User
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
+
+
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
 def properties_list(request):
-    properties=Property.objects.all()
-    serializer=PropertiesListSerializer(properties,many=True)
+    properties = Property.objects.all()
+    serializer = PropertiesListSerializer(properties, many=True)
 
-    return  JsonResponse({
-        'data':serializer.data
+    return JsonResponse({
+        'data': serializer.data
     })
 
-@api_view(['POST', 'FILES'])
+
+@api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
+def properties_detail(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    serializer = PropertiesDetailSerializer(property, many=False)
+
+    return JsonResponse(serializer.data)
+
+
+@api_view(['POST', 'FILES'])
 def create_property(request):
     form = PropertyForm(request.POST, request.FILES)
 
--- hotel_backend/property/forms.py
+++ hotel_backend/property/forms.py
@@ -1,10 +1,12 @@
 from django.forms import ModelForm
 
 from .models import Property
+
+
 class PropertyForm(ModelForm):
     class Meta:
-        model=Property
-        fields=(
+        model = Property
+        fields = (
             'title',
             'description',
             'price_per_night',
